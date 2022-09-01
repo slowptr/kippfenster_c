2125df9e848a4558c3e4a8d0a23308e32a59f3f0
slowptr <42156104+slowptr@users.noreply.github.com>
2022-09-02 00:42:55 +0200

    Try to make the creation of window-controls more readable

--- src/kf_wnd_ctrls.c
+++ src/kf_wnd_ctrls.c
@@ -22,6 +22,16 @@ add_item (kf_ctrl_item_t item)
   cur_item_list_idx++;
 }
 HWND
+make_and_add_item (const char *identifier, HWND handle)
+{
+  kf_ctrl_item_t item;
+  item.identifier = identifier;
+  item.handle = handle;
+
+  add_item (item);
+  return item.handle;
+}
+HWND
 kf_wnd_ctrls_get_by_id (const char *identifier)
 {
   for (int i = 0; i < MAX_ITEMS; i++)
@@ -36,53 +46,41 @@ HWND
 kf_wnd_ctrls_add_button (kf_wnd_t *wnd, const char *identifier,
                          const char *text, int *pos, int *size)
 {
-  kf_ctrl_item_t item;
-  item.identifier = identifier;
-  item.handle = CreateWindow (
+  HWND handle = CreateWindow (
       WC_BUTTON, text, WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
       pos[0], pos[1], size[0], size[1], wnd->hwnd, NULL, wnd->wc.hInstance,
       NULL);
 
-  add_item (item);
-  return item.handle;
+  return make_and_add_item (identifier, handle);
 }
 HWND
 kf_wnd_ctrls_add_combobox (kf_wnd_t *wnd, const char *identifier, int *pos,
                            int *size)
 {
-  kf_ctrl_item_t item;
-  item.identifier = identifier;
-  item.handle = CreateWindow (
+  HWND handle = CreateWindow (
       WC_COMBOBOX, "", WS_VISIBLE | WS_CHILD | CBS_DROPDOWN | CBS_HASSTRINGS,
       pos[0], pos[1], size[0], size[1], wnd->hwnd, NULL, wnd->wc.hInstance,
       NULL);
 
-  add_item (item);
-  return item.handle;
+  return make_and_add_item (identifier, handle);
 }
 HWND
 kf_wnd_ctrls_add_groupbox (kf_wnd_t *wnd, const char *identifier, int *pos,
                            int *size)
 {
-  kf_ctrl_item_t item;
-  item.identifier = identifier;
-  item.handle = CreateWindow (
+  HWND handle = CreateWindow (
       WC_BUTTON, "", WS_VISIBLE | WS_CHILD | BS_GROUPBOX, pos[0], pos[1],
       size[0], size[1], wnd->hwnd, NULL, wnd->wc.hInstance, NULL);
 
-  add_item (item);
-  return item.handle;
+  return make_and_add_item (identifier, handle);
 }
 HWND
 kf_wnd_ctrls_add_listbox (kf_wnd_t *wnd, const char *identifier, int *pos,
                           int *size)
 {
-  kf_ctrl_item_t item;
-  item.identifier = identifier;
-  item.handle = CreateWindow (
+  HWND handle = CreateWindow (
       WC_LISTBOX, "", WS_VISIBLE | WS_CHILD | LBS_HASSTRINGS, pos[0], pos[1],
       size[0], size[1], wnd->hwnd, NULL, wnd->wc.hInstance, NULL);
 
-  add_item (item);
-  return item.handle;
+  return make_and_add_item (identifier, handle);
 }
